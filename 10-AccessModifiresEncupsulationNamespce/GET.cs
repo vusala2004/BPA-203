ce8ac0465b2b7e4208586b85f4f0b97a601e6424
vusala <vusalavk-bpa203@code.edu.az>
2025-10-22 11:24:56 +0400

    accessmodifires

---
+++ 10-AccessModifiresEncupsulationNamespce/GET.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_AccessModifiresEncupsulationNamespce
+{
+    internal class GET
+    {
+        private int _horsePower;
+        public int HorsePower
+        {
+            get
+            {
+                return _horsePower;
+            }
+            set
+            {
+                if (value < 100)
+                {
+                    Console.WriteLine("pleace set correct power");
+                    return;
+                }
+                _horsePower = value;
+            }
+        }
+    }
+}
---
+++ 10-AccessModifiresEncupsulationNamespce/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _10_AccessModifiresEncupsulationNamespce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GET get=new GET();
+            get.HorsePower = 200;
+            Console.WriteLine(get.HorsePower);
+        }
+    }
+}
